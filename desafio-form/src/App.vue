cb17e24daaa7f1d36ebf96cbd25adcfe89bd0def
pjelelhml <paulo3385@hotmail.com>
2021-02-04 14:05:05 -0400

    desafio formulario + componente nomecompleto concluido

---
+++ desafio-form/src/App.vue
@@ -0,0 +1,133 @@
+<template>
+  <div id="app">
+    <h1>Formulário Desafio</h1>
+    <div class="conteudo">
+      <form class="painel" v-if="!enviado">
+        <div class="cabecalho">Formulário</div>
+        <!-- Nome completo (Nome e Sobrenome) -->
+        <NomeCompleto v-model="usuario.nomeCompleto" />
+        <!-- Email -->
+        <Rotulo nome="Email">
+          <input type="text" v-model.lazy.trim="usuario.email" />
+        </Rotulo>
+        <!-- Senha -->
+        <Rotulo nome="Senha">
+          <input type="password" v-model="usuario.senha" />
+        </Rotulo>
+        <!-- armazenarDados Dados? (Sim/Não) -->
+        <Rotulo nome="Armazenar Dados?">
+          <input type="checkbox" v-model="armazenarDados" />
+        </Rotulo>
+        <hr />
+
+        <button @click.prevent="enviar">Enviar</button>
+
+        <!-- Exercicio 03 -->
+        <!-- Crie um componente personalizado NomeCompleto -->
+        <!-- Esse componente deve receber Nome e Sobrenome -->
+      </form>
+
+      <!-- Só mostrar o fomulário de não tiver sido submetido -->
+      <!-- Mostrar a área de Resultado apenas quando o formulário for submetido -->
+      <div class="painel" v-else>
+        <div class="cabecalho">Resultado</div>
+        <Rotulo nome="Nome">
+          <span>{{ usuario.nomeCompleto.nome }}</span>
+        </Rotulo>
+        <Rotulo nome="Sobrenome">
+          <span>{{ usuario.nomeCompleto.sobrenome }}</span>
+        </Rotulo>
+        <Rotulo nome="Email">
+          <span>{{ usuario.email }}</span>
+        </Rotulo>
+        <Rotulo nome="Senha">
+          <span>{{ usuario.senha }}</span>
+        </Rotulo>
+        <Rotulo nome="Armazenar Dados?">
+          <span>{{ armazenarDados }}</span>
+        </Rotulo>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Rotulo from "./components/Rotulo.vue";
+import NomeCompleto from "./components/NomeCompleto.vue";
+
+export default {
+  name: "app",
+  components: { Rotulo, NomeCompleto },
+  data() {
+    return {
+      usuario: { nomeCompleto: {} },
+      dado: 1,
+      armazenarDados: true,
+      enviado: false,
+    };
+  },
+  methods: {
+    enviar() {
+      this.enviado = true;
+    },
+  },
+};
+</script>
+
+<style>
+body {
+  background-color: #ececec;
+}
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+
+  display: flex;
+  flex-direction: column;
+}
+
+.conteudo {
+  display: flex;
+}
+
+.painel {
+  flex: 1;
+  background: #fff;
+  margin: 0px 10px;
+  padding: 20px;
+  border: 1px solid #aaa;
+  border-radius: 5px;
+}
+
+.painel .cabecalho {
+  width: 100%;
+  background-color: #ddd;
+  padding: 10px 0px;
+  border-radius: 5px;
+  font-size: 1.4rem;
+}
+
+#app form button {
+  float: right;
+  margin: 10px 0px;
+  padding: 10px 20px;
+  font-size: 1.4rem;
+  border-radius: 5px;
+  color: #fff;
+  background-color: #2196f3;
+}
+
+#app h1 {
+  font-weight: 200;
+  margin: 20px;
+  padding: 0;
+}
+
+.mr-4 {
+  margin-right: 40px;
+}
+</style>
