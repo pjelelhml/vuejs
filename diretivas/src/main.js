68d7d5145a00bc33250e97e855338c82c22824a3
pjelelhml <paulo3385@hotmail.com>
2021-02-05 13:04:46 -0400

    terminado praticas de diretivas

--- diretivas/src/main.js
+++ diretivas/src/main.js
@@ -4,7 +4,20 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 Vue.directive('destaque', {
+	bind(el, binding, vnode) {
+		// el.style.backgroundColor = 'lightgreen'
 
+		let atraso = 0
+		if (binding.modifiers['atrasar']) atraso = 3000
+
+		setTimeout(() => {
+			if (binding.arg === 'fundo') {
+				el.style.backgroundColor = binding.value
+			} else {
+				el.style.color = binding.value
+			}
+		}, atraso)
+	}
 })
 
 new Vue({
