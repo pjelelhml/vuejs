1d527d6a366bb335cd2020e8488f690b3f9b27bb
pjelelhml <paulo3385@hotmail.com>
2021-02-13 11:11:44 -0400

    subindo vue transiçoes

---
+++ animacoes-exercicios/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app" class="container-fluid">
+    <h1>Animações</h1>
+    <hr />
+    <b-button @click="exibir = !exibir" variant="primary" class="mb-4"
+      >Mostrar Mensagem
+    </b-button>
+
+    <transition>
+      <b-alert variant="info" show v-if="exibir">
+        {{ msg }}</b-alert
+      ></transition
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "Uma mensagem de info para o user!",
+      exibir: false,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 1.5rem;
+}
+</style>
