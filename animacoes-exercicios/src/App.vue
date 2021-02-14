21d2b169666504ec938cde1de1d156449a7c5bc7
pjelelhml <paulo3385@hotmail.com>
2021-02-14 11:58:26 -0400

    subindo animaçoes e efeitos de transiçao

--- animacoes-exercicios/src/App.vue
+++ animacoes-exercicios/src/App.vue
@@ -6,8 +6,14 @@
       >Mostrar Mensagem
     </b-button>
 
-    <transition>
+    <transition name="fade">
       <b-alert variant="info" show v-if="exibir">
+        {{ msg }}</b-alert
+      ></transition
+    >
+
+    <transition name="slide" type="animation">
+      <b-alert variant="info" show v-show="exibir">
         {{ msg }}</b-alert
       ></transition
     >
@@ -34,5 +40,56 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
   font-size: 1.5rem;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 2s;
+}
+
+.fade-enter-to {
+  opacity: 1;
+}
+
+.fade-leave {
+  opacity: 1;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateY(40px);
+  }
+  to {
+    transform: translateY(0);
+  }
+}
+
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+  }
+  to {
+    transform: translateY(40px);
+  }
+}
+
+.slide-enter-active {
+  animation: slide-in 2s ease;
+  transition: opacity 2s;
+}
+
+.slide-leave-active {
+  animation: slide-in 2s ease;
+  transition: opacity 2s;
+}
+
+.slide-enter,
+.slide-leave-to {
+  opacity: 0;
 }
 </style>
